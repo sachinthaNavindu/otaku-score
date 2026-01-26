9ce3ad968a48bf546635623b9809faba443b8601
Sachintha <navindus485@gmail.com>
2026-01-26 14:23:49 +0530

    Set up initial Expo project structure

---
+++ app/index.tsx
@@ -0,0 +1,13 @@
+import { Text, View } from "react-native"
+import "../global.css"
+
+
+const App = () => {
+    return (
+        <View>
+            <Text>Hello ,otaku score</Text>
+        </View>
+    )
+}
+
+export default App
